7bc8836bb95041678dcced34d40a230275cfae39
Sogol Hedayati <sogol.hdt@gmail.com>
2024-09-16 17:55:04 +0330

    qq

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 
 // apis for main site
-Route::prefix('v1')->group(base_path('routes/site/api_v1.php'));
+//Route::prefix('v1')->group(base_path('routes/site/api_v1.php'));
 // Route::prefix('v2')->group(base_path('routes/site/api_v2.php'));
+
+Route::get('/', function () {
+    return 'hii';
+});
